5aae710a10f2a4667eb8595ac196f2efa8ccfc35
Agustín Tuosto <agustintuosto@outlook.com>
2022-05-17 21:36:25 -0300

    Creado modal

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('prueba').addEventListener('click', function(){
+    document.querySelector('.popup-container').style.display = 'flex';
+});
+
+document.getElementById('prueba2').addEventListener('click', function(){
+    document.querySelector('.popup-container').style.display = 'none';
+});
