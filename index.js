d598dbe2cd1679f31db8ad11620c8f10da070529
Agustín Tuosto <agustintuosto@outlook.com>
2022-05-18 22:30:44 -0300

    Agregados algunos planetas

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-document.getElementById('prueba').addEventListener('click', function(){
-    document.querySelector('.popup-container').style.display = 'flex';
-});
+document.getElementById("video-sol").addEventListener("click", function(){
+    var popup = document.getElementById("popup");
+    popup.style.display = 'flex';
+    popup.insertAdjacentHTML("afterend", '<iframe class="video" src="https://www.youtube.com/embed/LkaLfbuB_6E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+})
 
-document.getElementById('prueba2').addEventListener('click', function(){
+document.getElementById("video-mercurio").addEventListener("click", function(){
+    var popup = document.getElementById("popup");
+    popup.style.display = 'flex';
+    popup.insertAdjacentHTML("afterend", '<iframe class="video" src="https://www.youtube.com/embed/TbfvCXeRXqs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+})
+
+document.getElementById("video-venus").addEventListener("click", function(){
+    var popup = document.getElementById("popup");
+    popup.style.display = 'flex';
+    popup.insertAdjacentHTML("afterend", '<iframe class="video" src="https://www.youtube.com/embed/rk0PZ1qnLXw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+})
+
+document.getElementById("video-tierra").addEventListener("click", function(){
+    var popup = document.getElementById("popup");
+    popup.style.display = 'flex';
+    popup.insertAdjacentHTML("afterend", '<iframe class="video" src="https://www.youtube.com/embed/jdkMHkF7BaA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+})
+
+document.getElementById("video-marte").addEventListener("click", function(){
+    var popup = document.getElementById("popup");
+    popup.style.display = 'flex';
+    popup.insertAdjacentHTML("afterend", '<iframe class="video" src="https://www.youtube.com/embed/sioX2bbkZms" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+})
+
+document.getElementById("video-jupiter").addEventListener("click", function(){
+    var popup = document.getElementById("popup");
+    popup.style.display = 'flex';
+    popup.insertAdjacentHTML("afterend", '<iframe class="video" src="https://www.youtube.com/embed/CC7OJ7gFLvE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+})
+
+document.getElementById('cerrar').addEventListener('click', function(){
+    var video = document.getElementsByClassName("video");
+    video[0].remove();
     document.querySelector('.popup-container').style.display = 'none';
 });
+
+
